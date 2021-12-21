3a1444e9f68fdb7217a7f6ed7fa0a800f2a90301
FernandoFlores <93888544+Fernando-Flores-Flores@users.noreply.github.com>
2021-12-21 10:37:29 -0400

    consumiendo API

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
-
   private _historial: string[] = [];
+  public resultados:any[]=[];
   get historial() {
     return [...this._historial];
   }
   buscarGifs(query: string) {
-    query=query.trim().toLowerCase();
-    if(!this._historial.includes(query)){
+    query = query.trim().toLowerCase();
+    if (!this._historial.includes(query)) {
       this._historial.unshift(query);
-      this._historial = this._historial.splice(0,10);
+      this._historial = this._historial.splice(0, 10);
     }
     console.log(this._historial);
+
+    //CONSUMIR APP
+
+    this.http
+      .get(`https://api.giphy.com/v1/gifs/search?api_key=hUr68gaMjWSnOn0V1L87K3lwrpC2hF1O&q=${query}&limit=10`
+      )
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
   }
+  //LLave de la API
+  private apiKey: string = 'hUr68gaMjWSnOn0V1L87K3lwrpC2hF1O';
+
+  constructor(private http: HttpClient) {}
 }
